15cf3349c735965e5e2d8685145f0edfd4ef4f34
eroccowaruico <naohito@analog.bz>
2026-05-04 22:57:52 +0900

    追加

---
+++ RideIntercom/packages/Audio/Codec/Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Codec",
+    platforms: [
+        .iOS("26.4"),
+        .macOS("26.4"),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Codec",
+            targets: ["Codec"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Codec"
+        ),
+        .testTarget(
+            name: "CodecTests",
+            dependencies: ["Codec"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
